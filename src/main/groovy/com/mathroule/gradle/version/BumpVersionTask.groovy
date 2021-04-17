ace1786597e8b96e65f922b40050d7b195de810c
Mathieu Rul <mathroule@gmail.com>
2021-04-17 11:30:35 +0200

    Mark property as input

--- src/main/groovy/com/mathroule/gradle/version/BumpVersionTask.groovy
+++ src/main/groovy/com/mathroule/gradle/version/BumpVersionTask.groovy
@@ -1,11 +1,14 @@
 package com.mathroule.gradle.version
 
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
 
 class BumpVersionTask extends AbstractBumpVersionTask {
 
+    @Input
     def bump = project.hasProperty('bump') ? project.property('bump') : ''
 
+    @Input
     String versionPath = project.hasProperty('versionPath') ? project.property('versionPath') : ''
 
     BumpVersionTask() {
