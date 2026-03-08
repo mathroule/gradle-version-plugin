1b90239bd53263886746ecf48f7debc07bfdc269
Mathieu Rul <mathroule@gmail.com>
2026-03-08 09:36:07 +0100

    Replace Gradle task creation with registration

--- src/main/groovy/com/mathroule/gradle/version/GradleVersionPlugin.groovy
+++ src/main/groovy/com/mathroule/gradle/version/GradleVersionPlugin.groovy
@@ -6,10 +6,10 @@ import org.gradle.api.Project
 class GradleVersionPlugin implements Plugin<Project> {
 
     void apply(Project project) {
-        project.tasks.create('bumpVersion', BumpVersionTask)
-        project.tasks.create('bumpPatchVersion', BumpPatchVersionTask)
-        project.tasks.create('bumpMinorVersion', BumpMinorVersionTask)
-        project.tasks.create('bumpMajorVersion', BumpMajorVersionTask)
+        project.tasks.register('bumpVersion', BumpVersionTask)
+        project.tasks.register('bumpPatchVersion', BumpPatchVersionTask)
+        project.tasks.register('bumpMinorVersion', BumpMinorVersionTask)
+        project.tasks.register('bumpMajorVersion', BumpMajorVersionTask)
         project.extensions.add('versionPluginCode', Version.getVersionCode())
         project.extensions.add('versionPluginName', Version.getVersionName())
     }
